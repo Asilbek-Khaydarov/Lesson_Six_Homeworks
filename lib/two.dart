0221c0fe902becff151ea790c87cfacb469909df
Asilbek <asilbek.goal.ftbl@gmail.com>
2023-01-06 18:30:56 +0500

    first comment

---
+++ lib/two.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Two extends StatelessWidget {
+  const Two({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.orange,
+            title: const Text('Hint and Label TextField'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 50),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: const TextStyle(fontSize: 32, color: Colors.red),
+                  decoration: const InputDecoration(
+                      hintText: 'Enter your email',
+                      hintStyle: TextStyle(color: Colors.black, fontSize: 20),
+                      border: UnderlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                      ),
+                      prefixIcon: Icon(Icons.contacts),
+                      labelText: 'Username',
+                      labelStyle: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                margin: const EdgeInsets.only(top: 20),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(fontSize: 32, color: Colors.red),
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(fontSize: 20)),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
